e55c33b9e4bf4a5a3c8c06f399f2f02499679d2d
Joseph Fabisevich <mergesort@me.com>
2012-01-24 21:30:04 -0500

    Now requires chpwn's WeeLoader. Cleaned up the code.

--- Makefile
+++ Makefile
@@ -2,8 +2,8 @@ include theos/makefiles/common.mk
 
 BUNDLE_NAME = QuickNote
 QuickNote_FILES = QuickNoteController.m
-QuickNote_INSTALL_PATH = /System/Library/WeeAppPlugins/
-QuickNote_FRAMEWORKS = QuartzCore UIKit
+QuickNote_INSTALL_PATH = /Library/WeeLoader/Plugins/
+QuickNote_FRAMEWORKS = QuartzCore CoreGraphics UIKit
 
 
 include $(THEOS_MAKE_PATH)/bundle.mk
--- QuickNoteController.m
+++ QuickNoteController.m
@@ -1,41 +1,79 @@
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
-#import <QuartzCore/QuartzCore.h>
-#import <SpringBoard/SpringBoard.h>
 #import "BBWeeAppController-Protocol.h"
+#import <QuartzCore/QuartzCore.h>
 
 //@author: Joseph Fabisevich
-//@version: 1.0
+//@version: 1.1
 
-float VIEW_HEIGHT = 70.0f;
+static NSBundle *_QuickNoteWeeAppBundle = nil;
+
 float BUTTON_WIDTH = 76.0f;
-@interface UIImage (LolAdditions)
--(UIImage*)resizableImageWithCapInsets:(UIEdgeInsets)arg1;
- @end
 
-@interface QuickNoteController : NSObject <BBWeeAppController, UITextViewDelegate>
+@interface QuickNoteController: NSObject <BBWeeAppController, UITextViewDelegate> 
 {
 	UIView *_view;
-    UITextView *_textview;
-    UIButton *_killme;
-    UIImageView *bg;
-    BOOL editing;
+	UIImageView *_backgroundView;
+	UITextView *_textview;
 }
+
+@property (nonatomic, retain) UIView *view;
 
 @end
 
-@implementation QuickNoteController
 
-- (id)init
-{   if((self = [super init])){}    return self;   }
+@implementation QuickNoteController
+@synthesize view = _view;
+
++ (void)initialize {
+	_QuickNoteWeeAppBundle = [[NSBundle bundleForClass:[self class]] retain];
+}
+
+- (id)init {
+	if((self = [super init]) != nil) {
+		
+	} return self;
+}
+
+- (void)dealloc {
+	[_view release];
+	[_backgroundView release];
+	[_textview release];
+	[super dealloc];
+}
+
+-(void) viewDidDisappear
+{
+    _textview.frame = CGRectMake(0.0f, 0.0f, _view.frame.size.width-4, [self viewHeight]);
+    [_textview resignFirstResponder];   
+}
 
 - (void)textViewDidBeginEditing:(UITextView *)textView
 {
-    _killme.hidden = NO;
-    _textview.frame = CGRectMake(0, 0, _view.frame.size.width-BUTTON_WIDTH, VIEW_HEIGHT);
-    bg.frame = CGRectMake(0, 0, _view.frame.size.width-BUTTON_WIDTH, VIEW_HEIGHT);
-    editing = YES;
+    _textview.frame = CGRectMake(0, 0, _view.frame.size.width-BUTTON_WIDTH, [self viewHeight]);
+	
+	UIButton *_killme = [UIButton buttonWithType:UIButtonTypeCustom];
+	_killme.frame = CGRectMake(_view.frame.size.width-68, 3, 64, 64);
+	
+	[_killme addTarget:self action:@selector(removeDoneButton) forControlEvents:UIControlEventTouchDown];
+	
+	_killme.backgroundColor = [UIColor blackColor];
+	[_killme setTitleColor: [UIColor whiteColor] forState:UIControlStateNormal];
+	
+	[_killme setTitle:@"Done" forState:UIControlStateNormal];         
+	_killme.layer.cornerRadius = 10.0f;
+	[_view addSubview:_killme];
 }
+
+-(void) removeDoneButton
+{
+    _textview.frame = CGRectMake(0.0f, 0.0f, _view.frame.size.width-4, [self viewHeight]);
+
+    [_textview resignFirstResponder];
+
+	for(UIButton *b in _view.subviews)
+		if([b isKindOfClass: [UIButton class]])
+			[b removeFromSuperview];
+}
+
 
 - (void)textViewDidChange:(UITextView *)textView
 {
@@ -43,104 +81,52 @@ float BUTTON_WIDTH = 76.0f;
     [prefs setObject:_textview.text forKey:@"theText"];
 }
 
--(void) buttonClicked
-{
-    editing = NO;
-    _killme.hidden = YES;
-    _textview.frame = CGRectMake(0.0f, 0.0f, _view.frame.size.width-4, VIEW_HEIGHT);
-    bg.frame = CGRectMake(0.0f, 0.0f, _view.frame.size.width-4, VIEW_HEIGHT);   
-    [_textview resignFirstResponder];
+
+- (void)loadFullView {
+	
+	_textview = [[UITextView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, [self viewHeight])];
+	_textview.delegate = self;
+	_textview.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+
+	NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
+	
+	_textview.text = [prefs objectForKey: @"theText"] == nil ? @"Welcome to quicknote." : [prefs objectForKey:@"theText"];
+	
+	_textview.textColor = [UIColor whiteColor];
+	_textview.layer.cornerRadius = 8.0f;
+
+	_textview.backgroundColor = [UIColor clearColor];
+
+	[_view addSubview:_textview];
+	// Add subviews to _backgroundView (or _view) here.
 }
 
-- (UIView *)view
-{
-	if (!_view)
-	{
-        editing = NO;
-		_view = [[UIView alloc] init];
-        if([(SpringBoard *)[UIApplication sharedApplication] activeInterfaceOrientation] == UIInterfaceOrientationPortrait ||
-           [(SpringBoard *)[UIApplication sharedApplication] activeInterfaceOrientation] == UIInterfaceOrientationPortraitUpsideDown)
-            _view.frame = CGRectMake(2.0f, 0.0f, 316.0f, VIEW_HEIGHT);
-        else
-            _view.frame = CGRectMake(2.0f, 0.0f, 476.0f, VIEW_HEIGHT);            
-        
-        
-        UIImage *bgImg = [[UIImage imageWithContentsOfFile:@"/System/Library/WeeAppPlugins/StocksWeeApp.bundle/WeeAppBackground.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(35.0f, 4.0f, 35.0f, 4.0f)];
-        bg = [[UIImageView alloc] initWithImage:bgImg];
-        
-        if([(SpringBoard *)[UIApplication sharedApplication] activeInterfaceOrientation] == UIInterfaceOrientationPortrait ||
-           [(SpringBoard *)[UIApplication sharedApplication] activeInterfaceOrientation] == UIInterfaceOrientationPortraitUpsideDown)
-            bg.frame = CGRectMake(0.0f, 0.0f, 316.0f, VIEW_HEIGHT);
-        else
-            bg.frame = CGRectMake(0.0f, 0.0f, 476.0f, VIEW_HEIGHT);
-        
-        [_view addSubview:bg];
-        [bg release];
-        
-        _textview = [[UITextView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, VIEW_HEIGHT)];
-        _textview.delegate = self;
-        NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
-        if([prefs objectForKey: @"theText"] == nil)
-            [_textview setText:@"Welcome to quicknote."];
-        else
-            [_textview setText:[prefs objectForKey:@"theText"]];
-        
-        [_view addSubview:_textview];
-        _textview.textColor = [UIColor whiteColor];
-        _textview.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed: @"/System/Library/WeeAppPlugins/WeatherNotifications.bundle/weeapp_bg@2x.png"]];
-        _textview.layer.cornerRadius = 8.0f;
-    
-        _killme = [UIButton buttonWithType:UIButtonTypeCustom];
-        _killme.frame = CGRectMake(_view.frame.size.width-66, 2, 64, 64);
-      
-        [_killme addTarget:self action:@selector(buttonClicked) forControlEvents:UIControlEventTouchDown];
-        _killme.userInteractionEnabled = YES;
-        _killme.hidden = YES;
-        
-        _killme.backgroundColor = [UIColor blackColor];
-        [_killme setTitleColor: [UIColor whiteColor] forState:UIControlStateNormal];
-        [_killme setTitle:@"Done" forState:UIControlStateNormal];         
-        _killme.layer.cornerRadius = 10.0f;
-        [_view addSubview:_killme];
-	}
-
-	return _view;
+- (void)loadPlaceholderView {
+	// This should only be a placeholder - it should not connect to any servers or perform any intense data loading operations.
+	
+	// All widgets are 316 points wide. Image size calculations match those of the Stocks widget.
+	
+	_view = [[UIView alloc] initWithFrame:(CGRect){CGPointZero, {316.f, [self viewHeight]}}];
+	_view.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+	
+	UIImage *bgImg = [UIImage imageWithContentsOfFile:@"/System/Library/WeeAppPlugins/StocksWeeApp.bundle/WeeAppBackground.png"];
+	UIImage *stretchableBgImg = [bgImg stretchableImageWithLeftCapWidth:floorf(bgImg.size.width / 2.f) topCapHeight:floorf(bgImg.size.height / 2.f)];
+	_backgroundView = [[UIImageView alloc] initWithImage:stretchableBgImg];
+	_backgroundView.frame = CGRectInset(_view.bounds, 2.f, 0.f);
+	_backgroundView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+	[_view addSubview:_backgroundView];
 }
 
--(void) viewDidDisappear
-{
-    editing = NO;
-    _killme.hidden = YES;
-    _textview.frame = CGRectMake(0.0f, 0.0f, _view.frame.size.width-4, VIEW_HEIGHT);
-    bg.frame = CGRectMake(0.0f, 0.0f, _view.frame.size.width-4, VIEW_HEIGHT);   
-    [_textview resignFirstResponder];   
+- (void)unloadView {
+	[_view release];
+	_view = nil;
+	[_backgroundView release];
+	_backgroundView = nil;
+	// Destroy any additional subviews you added here. Don't waste memory :(.
 }
 
--(void) willRotateToInterfaceOrientation: (UIInterfaceOrientation) orientation
-{
-    if(orientation == UIInterfaceOrientationPortrait || orientation == UIInterfaceOrientationPortraitUpsideDown)
-        _view.frame = CGRectMake(2.0f, 0.0f, 316.0f, VIEW_HEIGHT);
-    else
-        _view.frame = CGRectMake(2.0f, 0.0f, 476.0f, VIEW_HEIGHT);
-
-    _killme.frame = CGRectMake(_view.frame.size.width-66, 2, 64, 64);
-
-    if(editing)
-    {
-        bg.frame = CGRectMake(0.0f, 0.0f, _view.frame.size.width-BUTTON_WIDTH, VIEW_HEIGHT);   
-        _textview.frame = CGRectMake(0.0f, 0.0f, _view.frame.size.width-BUTTON_WIDTH, VIEW_HEIGHT);
-    }
-    else
-    {
-        _textview.frame = CGRectMake(0.0f, 0.0f, _view.frame.size.width, VIEW_HEIGHT);
-        bg.frame = CGRectMake(0.0f, 0.0f, _view.frame.size.width, VIEW_HEIGHT);   
-    }
-    [_textview resignFirstResponder];
-}
-
-- (float)viewHeight
-{
-	return VIEW_HEIGHT;
+- (float)viewHeight {
+	return 70.0f;
 }
 
 @end
